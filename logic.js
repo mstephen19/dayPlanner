8e491c512aa9843b9533488d3929e9a8bca47661
Matt <matthiasvstephens@gmail.com>
2021-09-29 01:21:26 +0200

    add basic on-page elements

---
+++ logic.js
@@ -0,0 +1,19 @@
+let today = moment();
+
+//display current day
+$('#currentDay').text(today.format('DD, MMM, YYYY'));
+
+//display time function
+function displayTime() {
+  let todayOnInterval = moment();
+  let timeNow = todayOnInterval.format('HH[:]mm[:]ss');
+  $('#currentTime').text(timeNow);
+  console.log('is this working?');
+}
+
+//start interval once document loads
+$(document).ready(function () {
+  setInterval(displayTime, 500);
+});
+
+
